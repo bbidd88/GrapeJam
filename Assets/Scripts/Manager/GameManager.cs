00dd2d99c8c2d65a7eca03f5454ade60e3b80f28
bbidd88 <bbidd88@gmail.com>
2025-11-09 18:43:01 +0900

    GameManager에 골드, 경험치 증가 함수 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,14 +1,51 @@
+using System;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class GameManager : YManager
-{    
+public struct GameInfo
+{
+    public float StageElapsedTime;
+    public int Exp;
+    public int Gold;
+}
+
+public class GameManager : YManager, YIManagerUpdate
+{
+    private GameInfo Info;
+    public GameInfo GetInfo()    
+    {
+        return Info;
+    }
     public override void OnAwake()
     {
-
+        Info.StageElapsedTime = 0.0f;
+        Info.Exp = 0;
+        Info.Gold = 0;
     }
 
     public override void OnStart()
     {
         
+    }
+
+    public void OnUpdate()
+    {
+        //
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        Info.StageElapsedTime += Time.deltaTime;
+        // 
+    }
+
+    public void AddGold(int InGold)
+    {
+        Info.Gold += InGold;
+    }
+    public void AddExp(int InExp)
+    {
+        Info.Exp += InExp;
     }
 }
