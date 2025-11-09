28bfc554f7518116d8415be18114169bde8d06c6
Gorebob <seoa8455@gmail.com>
2025-11-09 22:23:06 +0900

    1초마다 총 3발씩 쏘게 변경

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -4,16 +4,25 @@ public class Player : YCharacter
 {
     [SerializeField] private Gun Gun;
     [SerializeField] private float ShootTime = 1f;
+    [SerializeField] private int OneTimeShootCount = 3;
+    [SerializeField] private float OneTimeShootInterval = 0.1f;
 
     private float LastShootTime = 0f;
+    private int ShootCount = 0;
 
     protected override void Update()
     {
         base.Update();
 
+        if (ShootCount != 0)
+            return;
+
         if (LastShootTime >= ShootTime)
         {
-            Shoot();
+            for (int i = 0; i < OneTimeShootCount; i++)
+            {
+                Invoke("Shoot", i * OneTimeShootInterval);
+            }
             LastShootTime = 0f;
         }
 
@@ -26,5 +35,10 @@ public class Player : YCharacter
             return;
 
         Gun.Shoot(Attack);
+        ShootCount++;
+        if (ShootCount >= OneTimeShootCount)
+        {
+            ShootCount = 0;
+        }            
     }
 }
