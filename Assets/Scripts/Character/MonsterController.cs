6d670d2e4f12e24deaa1c91a26b7cde18971f826
bbidd88 <bbidd88@gmail.com>
2025-11-09 23:04:35 +0900

    일정 거리 이상 멀어지면 몬스터 되돌아오게 변경

--- Assets/Scripts/Character/MonsterController.cs
+++ Assets/Scripts/Character/MonsterController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class MonsterController : MonoBehaviour
 {
     [SerializeField] private float MoveSpeed = 2f;
+    [SerializeField] private float ForceReturnDist = 10.0f;
 
     public void FixedUpdate()
     {
@@ -21,14 +22,21 @@ public class MonsterController : MonoBehaviour
         if (!playerCharacter || playerCharacter.IsDeath())
             return;
 
+
         Vector3 moveDirection = player.transform.position - transform.position;
         moveDirection.Normalize();
 
         var rigidbody = GetComponent<Rigidbody>();
         if (rigidbody != null)
         {
-            var move = moveDirection * MoveSpeed * Time.fixedDeltaTime;
-            rigidbody.MovePosition(rigidbody.position + move);
+            if (Vector3.Distance(player.transform.position, transform.position) > ForceReturnDist)
+            {
+                rigidbody.linearVelocity = Vector3.zero;
+            }
+                
+            var movePos = transform.position + moveDirection * MoveSpeed * Time.fixedDeltaTime;
+            rigidbody.MovePosition(movePos);
+            
         }
     }
 }
